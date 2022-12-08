44e0f286d3e0736dd8953a2296abdb2d82c1e697
Ricardo Pedroni <ricardo@endaoment.org>
2022-12-07 22:49:12 -0300

    chore: KnowledgeTest contract refactor for tests

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,40 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function start() external {
+        players.push(msg.sender);
+    }
+
+    function concatenate(
+        string memory _a,
+        string memory _b
+    ) public pure returns (string memory) {
+        return string(abi.encodePacked(_a, _b));
+    }
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
     }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable _to) external onlyOwner {
+        _to.transfer(address(this).balance);
+    }
+
+    receive() external payable {}
 }
